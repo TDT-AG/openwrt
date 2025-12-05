a3b092f3043205f5d72e70b76a84ee28bf37d7cd
Martin Schiller <ms@dev.tdt.de>
2025-12-05 14:32:40 +0100

    mxl/lgm: add kmod-usb-dwc3-of-simple to DEFAULT_PACKAGES
    This adds the DWC3 USB driver (and all dependencies) to the default
    packages of the Intel / MaxLinear Lightning Mountain target.
    
    Signed-off-by: Martin Schiller <ms@dev.tdt.de>

--- target/linux/mxl/lgm/target.mk
+++ target/linux/mxl/lgm/target.mk
@@ -3,6 +3,8 @@ SUBTARGET:=lgm
 BOARDNAME:=LGM
 FEATURES:=squashfs ext4 pci usb gpio boot-part rootfs-part emmc rtc
 
+DEFAULT_PACKAGES += kmod-usb-dwc3-of-simple
+
 define Target/Description
 	MaxLinear LGM
 endef
