305ac344e1420f2821c49a19456378b1e1d45249
Martin Schiller <ms@dev.tdt.de>
2026-05-08 09:21:11 +0200

    mxl: add support for TDT NG2000
    This adds support for the TDT NG2000 Router based on MxL URX850 +
    VRX619.
    
    It has 4x 2.5GE LAN + 1x 2.5GE WAN ports.
    
    It also supports a M.2 Key B (USB-only) socket for WWAN and M.2 Key E
    socket for WiFi.
    
    Signed-off-by: Martin Schiller <ms@dev.tdt.de>

--- target/linux/mxl/image/lgm.mk
+++ target/linux/mxl/image/lgm.mk
@@ -14,3 +14,20 @@ define Device/mxl_mxl25641-hdk-6
 	kmod-usb3
 endef
 TARGET_DEVICES += mxl_mxl25641-hdk-6
+
+define Device/tdt_luke
+  DEVICE_VENDOR := TDT
+  DEVICE_MODEL := NG2000
+  DEVICE_DTS := tdt_luke
+  DEVICE_PACKAGES := \
+	kmod-phy-lgm-usb \
+	kmod-usb-dwc3 \
+	kmod-usb-dwc3-of-simple \
+	kmod-usb3 \
+	kmod-eeprom-at24 \
+	kmod-fs-f2fs kmod-gpio-button-hotplug \
+	kmod-hwmon-lm90 \
+	kmod-hwmon-ina2xx \
+	kmod-leds-ktd206x kmod-rtc-ds1307 uboot-envtools
+endef
+TARGET_DEVICES += tdt_luke
